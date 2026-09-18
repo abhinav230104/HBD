913e89c7cfdb1c9abc08be06a78ead8b3d39e3f6
abhinav230104 <abhinavuniyal106@gmail.com>
2026-09-18 11:29:43 +0530

    Deploy set

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,24 @@
-cd HBD
+#!/bin/bash
+
+set -e
+
+echo "===== Deployment started ====="
+
+cd ~/HBD
+
+echo "===== Pulling latest code ====="
 git pull origin main
+
+echo "===== Installing dependencies ====="
 npm install
+
+echo "===== Building application ====="
 npm run build
-npm run start
+
+echo "===== Restarting application ====="
+pm2 restart hbd
+
+echo "===== Saving PM2 process ====="
+pm2 save
+
+echo "===== Deployment completed ====="
